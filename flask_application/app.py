85786fa2cfa08c25366d25cff0709ee288fc158b
vijayrohin <vijayrohinp@gmail.com>
2019-08-26 21:54:01 +1000

    world factbook data api readily deployed state.

--- flask_application/app.py
+++ flask_application/app.py
@@ -35,18 +35,99 @@ class WorldFactbookData(Resource):
     #         return 'Hello ' + request.args['name']
     #     else:
     #         return 'Hello John Doe'
+    #model = api.model('Model', {
+    #    'Region': fields.String,
+    #    'Age': fields.String,
+    #    'Time': fields.Integer,
+    #    'Person': fields.Integer,
+    #    'Male': fields.Integer,
+    #    'Female': fields.Integer,
+    #    'id': fields.Integer
+    #})
+    #@api.marshal_with(model, envelope='resource')
+    #def get(self):
+    #     if 'name' in request.args:
+    #         return WorldFactbook.query.filter_by(Region=request.args['name']).first()
+    #     else:
+    #        return WorldFactbook.query.filter_by(Region='Victoria').first()
+        #return WorldFactbook.query.filter_by(Region='Victoria').all()
     model = api.model('Model', {
-        'Region': fields.String,
-        'Age': fields.String,
-        'Time': fields.Integer,
-        'Person': fields.Integer,
-        'Male': fields.Integer,
-        'Female': fields.Integer,
+    #    'Region': fields.String,
+    #    'Age': fields.String,
+    #    'Time': fields.Integer,
+    #    'Person': fields.Integer,
+    #    'Male': fields.Integer,
+    #    'Female': fields.Integer,
+    #    'id': fields.Integer
+        "Administrative divisions": fields.String,
+        "Agriculture - products": fields.String,
+        "Background": fields.String,
+        "Climate": fields.String,
+        "Coastline": fields.String,
+        "Economy - overview": fields.String,
+        "Electricity - consumption": fields.String,
+        "Ethnic groups": fields.String,
+        "Flag description": fields.String,
+        "GDP - per capita (PPP)": fields.String,
+        "Geography - note": fields.String,
+        "Government type": fields.String,
+        "Independence": fields.String,
+        "Industries": fields.String,
+        "Internet country code": fields.String,
+        "Languages": fields.String,
+        "Legal system": fields.String,
+        "Location": fields.String,
+        "Map references": fields.String,
+        "Military branches": fields.String,
+        "Military service age and obligation": fields.String,
+        "National symbol(s)": fields.String,
+        "Natural resources": fields.String,
+        "Population": fields.String,
+        "Population below poverty line": fields.String,
+        "Pop growth rate": fields.String,
+        "Religions": fields.String,
+        "Taxes and other revenues": fields.String,
+        "Terrain": fields.String,
+        "adjective": fields.String,
+        "agriculture": fields.String,
+        "border countries": fields.String,
+        "consulate(s) general": fields.String,
+        "container port(s) (TEUs)": fields.String,
+        "conventional long form": fields.String,
+        "conventional short form": fields.String,
+        "dual citizenship recognized": fields.String,
+        "elderly dependency ratio": fields.String,
+        "elections/appointments": fields.String,
+        "elevation extremes": fields.String,
+        "forest": fields.String,
+        "geographic coordinates": fields.String,
+        "government consumption": fields.String,
+        "highest court(s)": fields.String,
+        "household consumption": fields.String,
+        "imports of goods and services": fields.String,
+        "industry": fields.String,
+        "inventory of registered aircraft operated by air carriers": fields.String,
+        "investment in fixed capital": fields.String,
+        "investment in inventories": fields.String,
+        "land": fields.String,
+        "major seaport(s)": fields.String,
+        "national song": fields.String,
+        "people": fields.String,
+        "potential support ratio": fields.String,
+        "revenues": fields.String,
+        "time difference": fields.String,
+        "urban population": fields.String,
+        "water": fields.String,
+        "youth dependency ratio": fields.String,
         'id': fields.Integer
     })
     @api.marshal_with(model, envelope='resource')
-    def get(self, **kwargs):
-        return WorldFactbook.query.filter_by(Region='Victoria').all()
+    def get(self):
+         if 'name' in request.args:
+             return WorldFactbook.query.filter_by(country_name=request.args['name']).first()
+         else:
+            return WorldFactbook.query.filter_by(country_name='Italy').first()
+        #return WorldFactbook.query.filter_by(Region='Victoria').all()
 
 @application.teardown_appcontext
 def shutdown_session(exception=None):
--- flask_application/models.py
+++ flask_application/models.py
@@ -18,24 +18,172 @@ blog_posts = Table('blog_posts', metadata,
 
 mapper(BlogPost, blog_posts)
 
+#class WorldFactbook(object):
+#	query = db_session.query_property()
+#	def __init__(self, region=None, age=None, time=None, person=None, male=None, female=None, id=None):
+#		self.region = region
+#		self.age = age
+#		self.time = time
+#		self.person = person
+#		self.male = male
+#		self.female = female
+#		self.id = id
+
+#world_factbook = Table('nom_child_data', metadata,
+#	Column('Region', Text),
+#    Column('Age', Text),
+#    Column('Time', Text),
+#    Column('Person', Text),
+#    Column('Male', Text),
+#    Column('Female', Text),
+#    Column('id', Integer, primary_key=True)
+#)
+
+#mapper(WorldFactbook, world_factbook)
+
 class WorldFactbook(object):
 	query = db_session.query_property()
-	def __init__(self, region=None, age=None, time=None, person=None, male=None, female=None, id=None):
-		self.region = region
-		self.age = age
-		self.time = time
-		self.person = person
-		self.male = male
-		self.female = female
+	def __init__(self, administrative_divisions=None, agriculture_products=None, background=None, climate=None, coastline=None, 
+		economy_overview=None, electricity_consumption=None, ethnic_groups=None, flag_description=None, gdp=None, geograpy=None, 
+		government_type=None, independence=None, industries=None, internet_country_code=None, languages=None, legal_system=None, 
+		location=None, map_reference=None, military_branches=None, military_service_age=None, national_symbol=None, natural_resources=None, 
+		population=None, population_below_poverty_line=None, population_growth=None, religions=None, taxes_and_revenues=None, terrain=None, 
+		people=None, agriculture=None, border_countries=None, consulates=None, container_port=None, conventional_long_form=None, 
+		conventional_short_form=None, dual_citizenship=None, elderly_dependancy_ratio=None, election_process=None, elevation=None, 
+		forest=None, geographic_location=None, government_consumption=None, highest_court=None, household_consumption=None, 
+		imports_goods_services=None, industry=None, inventory_registered_aircraft_operated=None, investment_fixed_capital=None, 
+		investment_inventories=None, land=None, major_seaports=None, national_song=None, people_plurals=None, potential_support_ratio=None, 
+		revenues=None, time_differences=None, urban_population=None, water=None, youth_dependency=None, id=None):
+		self.administrative_divisions = administrative_divisions
+		self.agriculture_products = agriculture_products
+		self.background = background
+		self.climate = climate
+		self.coastline = coastline
+		self.economy_overview = economy_overview
+		self.electricity_consumption = electricity_consumption
+		self.ethnic_groups = ethnic_groups
+		self.flag_description = flag_description
+		self.gdp = gdp
+		self.geograpy = geograpy
+		self.government_type = government_type
+		self.independence = independence
+		self.industries = industries
+		self.internet_country_code = internet_country_code
+		self.languages = languages
+		self.legal_system = legal_system
+		self.location = location
+		self.map_reference = map_reference
+		self.military_branches = military_branches
+		self.military_service_age = military_service_age
+		self.national_symbol = national_symbol
+		self.natural_resources = natural_resources
+		self.population = population
+		self.population_below_poverty_line = population_below_poverty_line
+		self.population_growth = population_growth
+		self.religions = religions
+		self.taxes_and_revenues = taxes_and_revenues
+		self.terrain = terrain
+		self.people = people
+		self.agriculture = agriculture
+		self.border_countries = border_countries
+		self.consulates = consulates
+		self.container_port = container_port
+		self.conventional_long_form = conventional_long_form
+		self.conventional_short_form = conventional_short_form
+		self.dual_citizenship = dual_citizenship
+		self.elderly_dependancy_ratio = elderly_dependancy_ratio
+		self.election_process = election_process
+		self.elevation = elevation
+		self.forest = forest
+		self.geographic_location = geographic_location
+		self.government_consumption = government_consumption
+		self.highest_court = highest_court
+		self.household_consumption = household_consumption
+		self.imports_goods_services = imports_goods_services
+		self.industry = industry
+		self.inventory_registered_aircraft_operated = inventory_registered_aircraft_operated
+		self.investment_fixed_capital = investment_fixed_capital
+		self.investment_inventories = investment_inventories
+		self.land = land
+		self.major_seaports = major_seaports
+		self.national_song = national_song
+		self.people_plurals = people_plurals
+		self.potential_support_ratio = potential_support_ratio
+		self.revenues = revenues
+		self.time_differences = time_differences
+		self.urban_population = urban_population
+		self.water = water
+		self.youth_dependency = youth_dependency
 		self.id = id
 
-world_factbook = Table('nom_child_data', metadata,
-	Column('Region', Text),
-    Column('Age', Text),
-    Column('Time', Text),
-    Column('Person', Text),
-    Column('Male', Text),
-    Column('Female', Text),
+world_factbook = Table('world_factbook_data', metadata,
+	#Column('Region', Text),
+    #Column('Age', Text),
+    #Column('Time', Text),
+    #Column('Person', Text),
+    #Column('Male', Text),
+    #Column('Female', Text),
+    #Column('id', Integer, primary_key=True)
+    Column("Administrative divisions", Text),
+    Column("Agriculture - products", Text),
+    Column("Background", Text),
+    Column("Climate", Text),
+    Column("Coastline", Text),
+    Column("Economy - overview", Text),
+    Column("Electricity - consumption", Text),
+    Column("Ethnic groups", Text),
+    Column("Flag description", Text),
+    Column("GDP - per capita (PPP)", Text),
+    Column("Geography - note", Text),
+    Column("Government type", Text),
+    Column("Independence", Text),
+    Column("Industries", Text),
+    Column("Internet country code", Text),
+    Column("Languages", Text),
+    Column("Legal system", Text),
+    Column("Location", Text),
+    Column("Map references", Text),
+    Column("Military branches", Text),
+    Column("Military service age and obligation", Text),
+    Column("National symbol(s)", Text),
+    Column("Natural resources", Text),
+    Column("Population", Text),
+    Column("Population below poverty line", Text),
+    Column("Pop growth rate", Text),
+    Column("Religions", Text),
+    Column("Taxes and other revenues", Text),
+    Column("Terrain", Text),
+    Column("adjective", Text),
+    Column("agriculture", Text),
+    Column("border countries", Text),
+    Column("consulate(s) general", Text),
+    Column("container port(s) (TEUs)", Text),
+    Column("conventional long form", Text),
+    Column("conventional short form", Text, key='country_name'),
+    Column("dual citizenship recognized", Text),
+    Column("elderly dependency ratio", Text),
+    Column("elections/appointments", Text),
+    Column("elevation extremes", Text),
+    Column("forest", Text),
+    Column("geographic coordinates", Text),
+    Column("government consumption", Text),
+    Column("highest court(s)", Text),
+    Column("household consumption", Text),
+    Column("imports of goods and services", Text),
+    Column("industry", Text),
+    Column("inventory of registered aircraft operated by air carriers", Text),
+    Column("investment in fixed capital", Text),
+    Column("investment in inventories", Text),
+    Column("land", Text),
+    Column("major seaport(s)", Text),
+    Column("national song", Text),
+    Column("people", Text),
+    Column("potential support ratio", Text),
+    Column("revenues", Text),
+    Column("time difference", Text),
+    Column("urban population", Text),
+    Column("water", Text),
+    Column("youth dependency ratio", Text),
     Column('id', Integer, primary_key=True)
 )
 
