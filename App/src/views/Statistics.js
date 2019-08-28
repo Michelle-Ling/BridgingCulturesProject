d3eba6a90b6ddfdca85885edb4ae12c09c3f5355
qshe0005 <qshe0005@student.monash.edu>
2019-08-28 16:34:58 +1000

    update the Tableau chart

--- App/src/views/Statistics.js
+++ App/src/views/Statistics.js
@@ -8,7 +8,7 @@ class Statistics extends Component {
     render() { 
       const placeholderStyle = {display: 'block', width: '1000px', height: '827px', margin: '0px', padding: '0px', border: 'none'};
       return (
-        <div><iframe src = "https://public.tableau.com/shared/NN2GWQS5P?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=yes&:loadOrderID=0"
+        <div><iframe src = "https://public.tableau.com/views/test_15667233981190/Dashboard3?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=no&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=yes&publish=yes&:loadOrderID=0"
             style={placeholderStyle}>
           </iframe>
           </div>
