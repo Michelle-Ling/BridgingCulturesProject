e0cb45b66adeaf16a38484fe1ddfe8d4070f8e96
qshe0005 <qshe0005@student.monash.edu>
2019-08-28 15:59:26 +1000

    update calendar

--- App/src/views/Calendar.js
+++ App/src/views/Calendar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
-
+import '../scss/Calendar.scss' 
 export default class Calendar extends React.Component {
 
     calendarComponentRef = React.createRef()
@@ -17,9 +17,9 @@ export default class Calendar extends React.Component {
       return (
         <div className='demo-app'>
           <div className='demo-app-top'>
-            <button onClick={ this.toggleWeekends }>toggle weekends</button>&nbsp;
-            <button onClick={ this.gotoPast }>go to a date in the past</button>&nbsp;
-            (also, click a date/time to add an event)
+            {/* <button onClick={ this.toggleWeekends }>toggle weekends</button>&nbsp; */}
+            {/* <button onClick={ this.gotoPast }>go to a date in the past</button>&nbsp; */}
+            
           </div>
           <div className='demo-app-calendar'>
             <FullCalendar
@@ -40,16 +40,16 @@ export default class Calendar extends React.Component {
       )
     }
   
-    toggleWeekends = () => {
-      this.setState({ // update a property
-        calendarWeekends: !this.state.calendarWeekends
-      })
-    }
+    // toggleWeekends = () => {
+    //   this.setState({ // update a property
+    //     calendarWeekends: !this.state.calendarWeekends
+    //   })
+    // }
   
-    gotoPast = () => {
-      let calendarApi = this.calendarComponentRef.current.getApi()
-      calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
-    }
+    // gotoPast = () => {
+    //   let calendarApi = this.calendarComponentRef.current.getApi()
+    //   calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+    // }
   
     // handleDateClick = (arg) => {
     //   if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
